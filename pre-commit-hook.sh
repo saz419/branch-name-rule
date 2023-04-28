6a374b26c3a4a930ec71245207a58396976ac0e2
saz419 <sarfaraz.ahamed@datasutram.com>
2023-04-28 15:43:17 +0530

    cc

--- pre-commit-hook.sh
+++ pre-commit-hook.sh
@@ -4,11 +4,9 @@ echo "Running pre-commit hook"
 
 BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD | sed 's/^refs\/heads\///')
 
-echo "Branch name: $BRANCH_NAME"
-
 if ! [[ $BRANCH_NAME =~ ^(feature|hotfix)\/[a-z0-9]+-[0-9]+-[a-z0-9-]+(-[a-z0-9-]+)*$
  ]]; then
-  echo "Invalid branch name. Branch name should be in the format of 'feature/{ticket_name}-{issue-description}' or 'hotfix/{ticket_name}-{issue-description}'"
+  echo "Invalid branch name [$BRANCH_NAME]. Branch name should be in the format of 'feature/{board-name}-{ticket-number}-{ticket-name}' or 'hotfix/{board-name}-{ticket-number}-{ticket-name}'"
   exit 1
 fi
 
